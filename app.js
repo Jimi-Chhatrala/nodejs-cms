ca0244b0f724d8886989911dc01a5cbfd5cb8bca
jimichhatrala <jimichhatrala8488@gmail.com>
2022-06-30 02:34:31 +0530

    4)MongoDB Connection

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
 const express = require("express");
+
 const mongoose = require("mongoose");
+
+const path = require("path");
+
+const app = express();
+
+const port = 8000;
+
+const pathDirectory = path.join(__dirname, "public");
+
+// Configure Mongoose to Connect to MongoDB
+
+mongoose
+  .connect("mongodb://localhost:27017/node-cms", {
+    useNewUrlParser: true,
+  })
+  .then((response) => {
+    console.log("MongoDB Connected Successfully");
+  })
+  .catch((err) => {
+    console.log("Database Connection Failed");
+  });
+
+// Configure Express
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(pathDirectory));
+app.listen(port, () => {
+  console.log(`Server is running on port : ${port}`);
+});
+
+// Routes
+app.use("/", (req, res) => {
+  res.send("Welcome to Node CMS App");
+});
